95ecdaa260b46297e437f9cb0dd2dc06fc16d61e
Markus Zehnder <business@markuszehnder.ch>
2024-02-26 11:33:55 +0100

    chore: fix linting issue from new black version

--- ucapi/api_definitions.py
+++ ucapi/api_definitions.py
@@ -192,9 +192,7 @@ class SetupComplete(SetupAction):
     """Setup action to complete a successful setup process."""
 
 
-CommandHandler: TypeAlias = Callable[
-    [Any, str, dict[str, Any] | None], Awaitable[StatusCodes]
-]
+CommandHandler: TypeAlias = Callable[[Any, str, dict[str, Any] | None], Awaitable[StatusCodes]]
 
 
 SetupHandler: TypeAlias = Callable[[SetupDriver], Awaitable[SetupAction]]
